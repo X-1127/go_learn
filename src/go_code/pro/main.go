e958bc630d899448a6d36d343e5709c3b9e9d0df
gittyty <3665802035@qq.com>
2026-07-25 11:47:20 +0800

    interface

--- src/go_code/pro/main.go
+++ src/go_code/pro/main.go
@@ -1,14 +1,29 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+)
 
-type class struct {
-	teacher string
-	student string
+type shape interface {
+	area() float64
+	perimeter() float64
+}
+
+type rect struct {
+	width, height float64
+}
+
+func (r *rect) area() float64 {
+	r.width += 1
+	return r.width * r.height
+}
+func (r *rect) perimeter() float64 {
+	return 2*r.height + 2*r.width
 }
 
 func main() {
-	c := class{teacher: "1", student: "2"}
+	square := rect{width: 5, height: 10}
 
-	fmt.Println(c.teacher)
+	fmt.Println(shape.area(&square))
+	fmt.Println((shape.perimeter(&square)))
 }
